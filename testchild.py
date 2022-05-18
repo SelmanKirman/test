08b7f31d2c3482bc866a09b4922a6bf1f1505b0c
Selman Kirman <selmankirman@hotmail.com>
2022-05-18 17:27:25 +0200

    first commit

--- testchild.py
+++ testchild.py
@@ -1,2 +1,6 @@
 ## Adding a new file in child branch
 print ("Inside Child Branch")
+print ("Inside Child Branch")
+print ("Inside Child Branch")
+print ("Inside Child Branch")
+print ("Inside Child Branch")
